008c8ef1035627019a631d8b00989a81e4215e36
Hakkology <yildizhakan88@gmail.com>
2024-02-02 11:21:59 +0300

    concurrency box, rc

--- src/concurrency.rs
+++ src/concurrency.rs
@@ -1,3 +1,12 @@
+use std::mem;
+use std::rc::Rc;
+
+#[derive(Debug)]
+enum Kamp{
+    Ders(String, i32),
+    Hoca(String)
+}
+
 fn main(){
 
     // Thread
@@ -12,5 +21,20 @@ fn main(){
     // WAR
 
     // Data Hazards and its Handling Methods
+
+    // Box <T> for allocating values on heap
+    // Rc<T> reference counting type that enables multiple ownership
+    // Arc<T>
+    // Ref<T>
+
+    // thread safe değil
+    let box_var = Box::new(Kamp::Ders(("Rust 102").to_string(), 10));
+    println!("{:?}", &box_var);
+    println!("{}", mem::size_of_val(&box_var).to_string());
+    //box_var.as_mut();
+
+    let rc_var = Rc::new(Kamp::Hoca("Emin Fedar".to_string()));
+    println!("{:?}", &rc_var);
+    println!("{}", mem::size_of_val(&rc_var));
 
 }
